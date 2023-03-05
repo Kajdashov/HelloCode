7ce8f13ec2d036843f9c9384be3361d3cc6fe399
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-03-05 22:47:37 +0300

    Add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "владимир"){
+
+    Console.WriteLine("Ура, это же Вова!!!");
+
+}
+    else{
+
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+
+    }
